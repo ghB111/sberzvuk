f52bcb08df825644b829c7f130081f3b398e295e
Roman <b111forjob@gmail.com>
2021-10-30 17:58:53 +0700

    enhance main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI, status
 import os
 import requests
 
+import config
+
 app = FastAPI()
 
 def download(url: str, dest_folder: str, filename: str):
@@ -26,20 +28,11 @@ def download(url: str, dest_folder: str, filename: str):
     else:  # HTTP status code 4XX/5XX
         print("Download failed: status code {}\n{}".format(r.status_code, r.text))
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
 @app.post("/recognise", status_code= status.HTTP_200_OK )
 def read_input(source: str, prefix:str):
     r = requests.get(source, allow_redirects=True)
-    dest_folder = "downloads"
+    dest_folder =  config.DOWNLOADS_FOLDER
     download(source, dest_folder, prefix)
-    return {"source": len(r.content), "prefix":prefix}
+    # return {"source": len(r.content), "prefix":prefix} # todo return message
 
 
