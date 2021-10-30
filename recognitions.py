b4c6cbde74778dd4f17785d051cf1612a3b25c7e
Roman <b111forjob@gmail.com>
2021-10-30 18:07:43 +0700

    fix type annot

--- recognitions.py
+++ recognitions.py
@@ -1,13 +1,14 @@
 import config
+from typing import Tuple
 
 # todo
-def do_machine_learning(path_to_video: str) -> tuple[str, str]:
+def do_machine_learning(path_to_video: str) -> Tuple[str, str]:
     return (
             '{"type": "audio"}',
             '{"type": "video"}'
             )
 
-def get_all_result_file_names(prefix: str) -> tuple[str, str, str]:
+def get_all_result_file_names(prefix: str) -> Tuple[str, str, str]:
     return (
             prefix + "_audio.json",
             prefix + "_video.json",
